10cfb2dda627f0b37c77dfbb7f97c48b546a844b
harshalkale0911 <hkale6888@gmail.com>
2024-06-02 14:34:37 +0530

    h

---
+++ Demo_app/backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const { connected } = require("./src/config/db")
+ 
+
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+
+app.listen(3000, async() => {
+   try{
+     await connected
+     console.log("Connected to DB")
+     console.log("Server is running on port 3000")
+   }
+   catch(err){
+    
+    console.log(err)
+   }
+})
