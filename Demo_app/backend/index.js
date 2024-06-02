c00c7be301332ee11a21351fc704a46da6b90608
harshalkale0911 <hkale6888@gmail.com>
2024-06-02 15:05:26 +0530

    h

--- Demo_app/backend/index.js
+++ Demo_app/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const { connected } = require("./src/config/db")
+const { connected } = require("./config/db")
+ 
  
 
 
@@ -17,7 +18,7 @@ app.listen(3000, async() => {
      console.log("Server is running on port 3000")
    }
    catch(err){
-    
+
     console.log(err)
    }
 })
